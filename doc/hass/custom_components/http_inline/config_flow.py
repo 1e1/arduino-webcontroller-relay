efb0da9776188a5b295e1a9a614ac09db4b24720
1e1 <2.71828183e0+git@gmail.com>
2020-08-31 23:43:06 +0200

    add switch custom names on HASS

--- doc/hass/custom_components/http_inline/config_flow.py
+++ doc/hass/custom_components/http_inline/config_flow.py
@@ -1,25 +1,25 @@
 """Config flow to configure the WebController-Relay integration."""
+
 import logging
 
 import requests
 import voluptuous as vol
 
 from homeassistant import config_entries
-from homeassistant.const import CONF_NAME, CONF_HOST, HTTP_OK
-from homeassistant.helpers import config_validation as cv
+from homeassistant.const import CONF_HOST, CONF_NAME, HTTP_OK
 
-from .const import ( # pylint: disable=unused-import
-    DOMAIN, 
-    CONF_PATH_PATTERN_READ, 
+from .const import (  # pylint: disable=unused-import
+    CONF_NB_SWITCHES,
+    CONF_PATH_PATTERN_READ,
     CONF_PATH_PATTERN_WRITE,
-    CONF_RELAY_ID_MIN,
-    CONF_RELAY_ID_MAX,
-    DEFAULT_HOST, 
-    DEFAULT_NAME, 
-    DEFAULT_PATH_PATTERN_READ, 
+    CONF_SWITCH_I_NAME_PATTERN,
+    CONF_SWITCH_NAMES,
+    DEFAULT_HOST,
+    DEFAULT_NAME,
+    DEFAULT_NB_SWITCHES,
+    DEFAULT_PATH_PATTERN_READ,
     DEFAULT_PATH_PATTERN_WRITE,
-    DEFAULT_RELAY_ID_MIN,
-    DEFAULT_RELAY_ID_MAX,
+    DOMAIN,
 )
 
 _LOGGER = logging.getLogger(__name__)
@@ -31,44 +31,83 @@ class HttpInlineFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
     VERSION = 1
     CONNECTION_CLASS = config_entries.CONN_CLASS_LOCAL_PUSH
 
-    def _data_schema(self, user_input=None):
+    def __init__(self):
+        """Initialize HTTP_inline config flow."""
+        self._host = None
+        self._name = None
+        self._path_read = None
+        self._path_write = None
+        self._nb_switches = 0
+        self._switch_names = []
+
+    def _global_data_schema(self, user_input=None):
         if user_input is None:
             user_input = {}
 
         return vol.Schema(
             {
-                vol.Required(CONF_HOST, default=user_input.get(CONF_HOST, DEFAULT_HOST)): str, #cv.url,
-                vol.Optional(CONF_NAME, default=user_input.get(CONF_NAME, DEFAULT_NAME)): str, #cv.string,
-                vol.Optional(CONF_PATH_PATTERN_READ, default=user_input.get(CONF_PATH_PATTERN_READ, DEFAULT_PATH_PATTERN_READ)): str, #cv.string,
-                vol.Optional(CONF_PATH_PATTERN_WRITE, default=user_input.get(CONF_PATH_PATTERN_WRITE, DEFAULT_PATH_PATTERN_WRITE)): str, #cv.string,
-                vol.Optional(CONF_RELAY_ID_MIN, default=user_input.get(CONF_RELAY_ID_MIN, DEFAULT_RELAY_ID_MIN)): int, #cv.positive_int,
-                vol.Optional(CONF_RELAY_ID_MAX, default=user_input.get(CONF_RELAY_ID_MAX, DEFAULT_RELAY_ID_MAX)): int, #cv.positive_int,
+                vol.Required(
+                    CONF_HOST, default=user_input.get(CONF_HOST, DEFAULT_HOST)
+                ): str,
+                vol.Required(
+                    CONF_NB_SWITCHES,
+                    default=user_input.get(CONF_NB_SWITCHES, DEFAULT_NB_SWITCHES),
+                ): int,
+                vol.Optional(
+                    CONF_NAME, default=user_input.get(CONF_NAME, DEFAULT_NAME)
+                ): str,
+                vol.Optional(
+                    CONF_PATH_PATTERN_READ,
+                    default=user_input.get(
+                        CONF_PATH_PATTERN_READ, DEFAULT_PATH_PATTERN_READ
+                    ),
+                ): str,
+                vol.Optional(
+                    CONF_PATH_PATTERN_WRITE,
+                    default=user_input.get(
+                        CONF_PATH_PATTERN_WRITE, DEFAULT_PATH_PATTERN_WRITE
+                    ),
+                ): str,
             }
         )
 
-    def _sanitize_user_input(self, user_input):
-        host = user_input[CONF_HOST]
-        name = user_input.get(CONF_NAME, user_input[CONF_HOST])
-        path_read = user_input.get(CONF_PATH_PATTERN_READ, DEFAULT_PATH_PATTERN_READ)
-        path_write = user_input.get(CONF_PATH_PATTERN_WRITE, DEFAULT_PATH_PATTERN_WRITE)
-        relay_id_min = user_input.get(CONF_RELAY_ID_MIN, DEFAULT_RELAY_ID_MIN)
-        relay_id_max = user_input.get(CONF_RELAY_ID_MAX, DEFAULT_RELAY_ID_MAX)
+    def _switches_data_schema(self, user_input=None):
+        if user_input is None:
+            user_input = {}
 
+        schema = vol.Schema({})
+        for i in range(0, self._nb_switches):
+            name = CONF_SWITCH_I_NAME_PATTERN.format(i)
+            default = f"{self._name} #{i}"
+
+            schema = schema.extend({vol.Optional(name, default=default): str})
+
+        return schema
+
+    def _export(self):
         return {
-            CONF_HOST: host, 
-            CONF_NAME: name, 
-            CONF_PATH_PATTERN_READ: path_read, 
-            CONF_PATH_PATTERN_WRITE: path_write, 
-            CONF_RELAY_ID_MIN: relay_id_min, 
-            CONF_RELAY_ID_MAX: relay_id_max,
+            CONF_HOST: self._host,
+            CONF_NAME: self._name,
+            CONF_PATH_PATTERN_READ: self._path_read,
+            CONF_PATH_PATTERN_WRITE: self._path_write,
+            CONF_NB_SWITCHES: self._nb_switches,
+            CONF_SWITCH_NAMES: self._switch_names,
         }
 
-    def _show_setup_form(self, user_input=None, errors=None):
+    def _show_global_setup_form(self, user_input=None, errors=None):
         """Show the setup form to the user."""
 
         return self.async_show_form(
             step_id="user",
-            data_schema=self._data_schema(user_input),
+            data_schema=self._global_data_schema(user_input),
+            errors=errors or {},
+        )
+
+    def _show_switches_setup_form(self, user_input=None, errors=None):
+        """Show the setup form to the user_bis."""
+        return self.async_show_form(
+            step_id="switch",
+            data_schema=self._switches_data_schema(user_input),
             errors=errors or {},
         )
 
@@ -77,7 +116,7 @@ class HttpInlineFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is None:
-            return self._show_setup_form(user_input, errors)
+            return self._show_global_setup_form(user_input, errors)
 
         await self.async_set_unique_id(user_input[CONF_HOST])
 
@@ -90,32 +129,66 @@ class HttpInlineFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         try:
             request = requests.get(user_input[CONF_HOST], timeout=10)
             if request.status_code == HTTP_OK:
-                return await self.async_step_import(user_input)
+                self._host = user_input[CONF_HOST]
+                self._name = user_input.get(CONF_NAME, user_input[CONF_HOST])
+                self._path_read = user_input.get(
+                    CONF_PATH_PATTERN_READ, DEFAULT_PATH_PATTERN_READ
+                )
+                self._path_write = user_input.get(
+                    CONF_PATH_PATTERN_WRITE, DEFAULT_PATH_PATTERN_WRITE
+                )
+                self._nb_switches = user_input[CONF_NB_SWITCHES]
 
-            _LOGGER.error("Server error {}".format(request.status_code))
+                return await self.async_step_switch()
+
+            _LOGGER.error("Server error %d", request.status_code)
             errors[CONF_HOST] = "Server error"
 
         except requests.exceptions.MissingSchema:
-            _LOGGER.error("Missing host or schema in configuration. Add http:// to your URL")
-            errors[CONF_HOST] = "Missing host or schema in configuration. Add http:// to your URL"
-            
+            _LOGGER.error(
+                "Missing host or schema in configuration. Add http:// to your URL"
+            )
+            errors[
+                CONF_HOST
+            ] = "Missing host or schema in configuration. Add http:// to your URL"
+
         except requests.exceptions.ConnectionError:
-            _LOGGER.error("No route to device at {}".format(user_input[CONF_HOST]))
+            _LOGGER.error("No route to device at %s", user_input[CONF_HOST])
             errors[CONF_HOST] = "No route to device"
 
         except Exception:  # pylint: disable=broad-except
             _LOGGER.exception(
-                "Unknown error connecting with server at {}".format(user_input[CONF_HOST])
+                "Unknown error connecting with server at %s", user_input[CONF_HOST]
             )
             errors["base"] = "unknown"
 
-        return self.async_show_form(step_id="user", errors=errors)
+        return self._show_global_setup_form(user_input, errors)
+
+    async def async_step_switch(self, user_input=None):
+        """Handle a flow initiated by the user."""
+        errors = {}
+
+        if user_input is None:
+            return self._show_switches_setup_form(user_input, errors)
+
+        self._switch_names = [None] * self._nb_switches
+
+        for i in range(0, self._nb_switches):
+            key = CONF_SWITCH_I_NAME_PATTERN.format(i)
+            if key in user_input:
+                self._switch_names[i] = user_input[key]
+            else:
+                return self._show_switches_setup_form(user_input, errors)
+
+        return await self.async_step_import(user_input)
 
     async def async_step_import(self, user_input=None):
         """Import a config entry."""
-        data = self._sanitize_user_input(user_input)
+        if user_input is None:
+            user_input = {}
+
+        data = self._export()
 
         self.hass.data[DOMAIN] = data
-        
-        await self.async_set_unique_id(data[CONF_HOST], raise_on_progress=False)
+
         return self.async_create_entry(title=data[CONF_NAME], data=data)
--- doc/hass/custom_components/http_inline/const.py
+++ doc/hass/custom_components/http_inline/const.py
@@ -1,18 +1,18 @@
 """Const for the http_inline integration."""
 
 
-DOMAIN = "HTTP_INLINE"
+DOMAIN = "http_inline"
 PLATFORMS = ["switch"]
 
 CONF_PATH_PATTERN_READ = "path_read"
 CONF_PATH_PATTERN_WRITE = "path_write"
-CONF_RELAYS = "relays"
-CONF_RELAY_ID_MIN = "first_relay_id"
-CONF_RELAY_ID_MAX = "last_relay_id"
+CONF_SWITCHES = "switches"
+CONF_NB_SWITCHES = "nb_switches"
+CONF_SWITCH_NAMES = "switch_names"
+CONF_SWITCH_I_NAME_PATTERN = "switch_name_{}"
 
 DEFAULT_HOST = "http://webrelay.local"
 DEFAULT_NAME = "http_inline switch"
-DEFAULT_PATH_PATTERN_READ = '/r/{relay_id}'
-DEFAULT_PATH_PATTERN_WRITE = '/w/{relay_id}/{state}'
-DEFAULT_RELAY_ID_MIN = '0'
-DEFAULT_RELAY_ID_MAX = '0'
+DEFAULT_PATH_PATTERN_READ = "/r/{switch_id}"
+DEFAULT_PATH_PATTERN_WRITE = "/w/{switch_id}/{state}"
+DEFAULT_NB_SWITCHS = "0"
