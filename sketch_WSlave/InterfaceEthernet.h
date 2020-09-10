488077e1edec4a208a39618162791f64e744b191
AGLR <Aymeric.Gerlier@brittany-ferries.fr>
2020-09-10 20:59:23 +0200

    write documentation remove dead code

--- sketch_WSlave/InterfaceEthernet.h
+++ sketch_WSlave/InterfaceEthernet.h
@@ -13,17 +13,6 @@
 
 
 
-#define ETH_SPI_D0_UNO    10
-#define ETH_SPI_D1_UNO    11
-#define ETH_SPI_D2_UNO    12
-#define ETH_SPI_D3_UNO    13
-#define ETH_SPI_D1_MEGA   50
-#define ETH_SPI_D2_MEGA   51
-#define ETH_SPI_D3_MEGA   52
-#define ETH_SPI_SS_MEGA   53
-#define ETH_SELECT_W5100  10
-#define ETH_SELECT_SD     4
-
 #define CRLF              "\r\n"
 #define HEADER_START      F("HTTP/1.0 200" CRLF "Content-Type: text/")
 #define HEADER_END_ACTION F("plain" CRLF CRLF)
